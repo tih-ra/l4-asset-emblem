9764807f04eb6e0e7d02ed252e100eba796a7929
Andriy Bazyuta <andriy.bazyuta@gmail.com>
2014-01-24 10:53:37 +0200

    filter

---
+++ tests/EmblemjsFilterTest.php
@@ -0,0 +1,9 @@
+<?php namespace Andriybazyuta\L4AssetEmblemjs;
+
+class EmblemjsFilterTest extends \PHPUnit_Framework_TestCase
+{	
+	public function testConstruction()
+	{
+		$filter = new Filters\EmblemjsFilter;
+	}
+}
